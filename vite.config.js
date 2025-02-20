30fc2bdbc72a5e9fe0adba11c7df9767775da608
zariqq <zkanylbay@mail.ru>
2025-02-20 22:56:18 +0700

    feat: gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
-import {fileURLToPath, URL} from 'url';
+import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/paint/dist/",
   plugins: [vue()],
   resolve: {
     alias: [
